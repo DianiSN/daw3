66ce01cfa6ab48f98ec3b5ff458354fc69756b68
a01320622 <a01320622@itesm.mx>
2014-11-14 09:32:00 -0600

    Start phase
    PD3

---
+++ displayArticulo.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script type="text/javascript" src="ajax.js"></script>
+		<title>Nuevo artículo</title>	
+	</head>
+	<header></header>
+	<body>
+
+		<nav>
+
+			<ul>
+				<li><a href="">Dondé va el menu</a></li>
+			</ul>
+
+		</nav>
+		<h2>Articulos </h2>
+		<div id="response" align="center">
+			<?php
+
+				// include_once('connection.php');
+				// $result = mysqli_query($connection,"SELECT Articulo.nombre,estadoArticulo,descripcion,calle FROM Articulo");
+			 //    echo "<table>";
+			 //    while($row = mysqli_fetch_array($result)) {
+			 //      echo "<tr>";
+			 //      echo "<td>" . $row['nombre'] . "</td>";
+			 //      echo "<td>" . $row['estadoArticulo'] . "</td>";
+			 //      echo "<td>" . $row['descripcion'] . "</td>";
+			 //      echo "<td>" . $row['calle'].", ".$row['cuidad'].", ".$row['state'].", ".$row['codigoPostal']."</td>";
+			 //      if($row['vendido'] == 0){
+			 //        echo "<td>Disponible</td>";
+
+			 //      }else{
+			 //         echo "<td>Vendido</td>";
+			 //      }
+			      
+			 //      echo "</tr>";
+			 //    }
+
+			 //    echo "</table>";
+			$url = "http://localhost/DAW/PD3/slim/display/articulos/"+$userId; //Route to the REST web service
+			$c = curl_init($url);
+			$response = curl_exec($c);
+			curl_close($c);
+
+			?>
+		
+		</div>
+	</body>
+</html>
---
+++ insertArticulo.php
@@ -0,0 +1,29 @@
+<?php
+include_once('connection.php');
+
+//escape variables for security
+$articulo = mysqli_real_escape_string($connection, $_POST['articulo']);
+$estArticulo = mysqli_real_escape_string($connection, $_POST['estArticulo']);
+$descripcion = mysqli_real_escape_string($connection, $_POST['descripcion']);
+$calle = mysqli_real_escape_string($connection, $_POST['calle']);
+$estado=mysqli_real_escape_string($connection, $_POST['estados']);
+$cuidad=mysqli_real_escape_string($connection, $_POST['cuidad']);
+$cp=mysqli_real_escape_string($connection, $_POST['codigoPostal']);
+
+if (preg_match("/^[.áéíóúa-zA-Z0-9 ]*$/",$calle) && preg_match("/^[áéíóúa-zA-Z ]*$/",$cuidad) && preg_match("/^[0-9]*$/",$cp)) {
+  $sql="INSERT INTO Articulo (usuarioId, nombre, estadoArticulo, descripcion, calle, state, cuidad, codigoPostal,vendido)
+  VALUES ('testUser1','$articulo', '$estArticulo', '$descripcion','$calle', '$estado', '$cuidad', '$cp','0')";
+
+  if (!mysqli_query($connection,$sql)) {
+    die('Error: ' . mysqli_error($connection));
+  }else{
+    $userId = 'testUser1';
+    include('displayArticulo.php');
+  }
+        
+
+}else{
+
+  echo "Por favor llenar la forma correctamente.\n Regrese a la página anterior para hacer correcciones.";
+      
+}
