41e9c779b1d48610aa91a83dfc5a18217f7750ca
rulgf <rulgf.420@gmail.com>
2014-11-18 16:58:26 -0600

    Login facebook

---
+++ main_login.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<form name="form1" method="post" action="checklogin.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td colspan="3"><strong>Member Login </strong></td>
+</tr>
+<tr>
+<td width="78">Username</td>
+<td width="6">:</td>
+<td width="294"><input name="myusername" type="text" id="myusername"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td>:</td>
+<td><input name="mypassword" type="password" id="mypassword"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Login"></td>
+</tr>
+</table>
+</td>
+</form>
+<form name="form2" method="post" action="register.php">
+<td><input type="submit" name="Register" value="Register"></td>
+</form>
+<td>
+<script src="fb.js"></script>
+
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+</td>
+</tr>
+</table>
+<button onclick="fbId()">Click me</button>
