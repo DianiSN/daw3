674696d41ed9e01ac49b49e41e5225854022401e
a01320622 <a01320622@itesm.mx>
2014-11-24 23:59:02 -0600

    Agregue precio a viewArticuloInfo

--- viewArticuloInfo.php
+++ viewArticuloInfo.php
@@ -8,10 +8,10 @@
 
 				
 				
-				$query = "SELECT `Articulo`.`vendido`, `Articulo`.`estadoArticulo`,`Articulo`.`descripcion`, `Articulo`.`calle`, `Estados`.`nombre` AS Estado, `Articulo`.`cuidad`, `Articulo`.`codigoPostal` 
+				$query = "SELECT `Articulo`.`nombre`,`Articulo`.`vendido`, `Articulo`.`estadoArticulo`,`Articulo`.`precio`,`Articulo`.`descripcion`, `Articulo`.`calle`, `Estados`.`nombre` AS Estado, `Articulo`.`cuidad`, `Articulo`.`codigoPostal` 
 				FROM Articulo, Estados WHERE `Articulo`.`state` = `Estados`.`estadoId` AND `Articulo`.`articuloId`=".$articuloId." ORDER BY `Articulo`.`vendido` ASC" ;
 				$getMail="SELECT email FROM usuarios WHERE fbId=".$userId;
-				echo "quesry:".$getMail;
+				//echo "quesry:".$getMail;
 
 		        $result = mysqli_query($connection,$query);
 		        $resultMail = mysqli_query($connection,$getMail);
@@ -25,8 +25,9 @@
 		        	echo "<h2>" . $row['nombre'] . "</h2>";
 		          
 		          echo "<h3> Estado del articulo:</h3>" . $row['estadoArticulo']."<br>";
+		          echo "<h3> Precio:</h3>  $" . $row['precio']."   MXN<br>";
 		          echo "<h3>Descripción:</h3>".$row['descripcion']."<br>";
-		          echo "<h3>Ubicación:</h3>" . $row['calle'] . ", ".$row['cuidad']. ",". $row['Estado']." C.P".$row['codigoPostal'];
+		          echo "<h3>Ubicación:</h3>" . $row['calle'] . ", ".$row['cuidad']. ",". $row['Estado']." C.P: ".$row['codigoPostal'];
 		          
 		          $direcc= $row['calle'].",".$row['cuidad'].",".$row['Estado'].",México";
 		          // echo "<p>".$direcc."</p>";
@@ -36,7 +37,7 @@
 							frameborder=\"0\" style=\"border:0\"
 							src=\"https://www.google.com/maps/embed/v1/search?key=AIzaSyCgpoQUbukp62MU1dzkpglRN6bd1e_ZYW0&q=".$direcc."\">
 						</iframe><br>";
-					echo "<br><br><br><br><br><a class=\"pure-button pure-button-primary\" id=\"email\" >Contactá vendedor</a><br><br><br><br><br>";
+					echo "<br><br><br><br><br><a class=\"pure-button pure-button-primary\" id=\"email\" href=\"mailto:".$mail."?Subject=".$row['nombre']."  $".$row['precio']." MXN\" >Contactá vendedor</a><br><br><br><br><br>";
 					//href=\"mailto:".$mail."?subject=".$row['nombre']."\"
 		          
 		        }
